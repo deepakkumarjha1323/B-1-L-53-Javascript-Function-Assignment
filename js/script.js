43ec52bb39e82fa670443384bff3be2d19486359
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-15 14:57:30 +0530

    L-53 assignment

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Functions
+
+// Addition
+// Function Defination
+function add(a,b){
+    console.log(a+b);
+}
+// Function Invoke/Call
+add(20,10);
+
+// Subtraction
+// Function defination
+function Subtraction(c,d){
+    //Function Body
+    // Every Function Return somethigs
+    return c - d;
+}
+// Function Calling/Invoking
+var sub = Subtraction(30,15);
+console.log(sub);
+
+// Multiplication
+// Function defination
+// Fat Arrow Function
+let multi=(e,f)=>{
+    console.log(e*f);
+}
+// Function calling
+multi(5,6);
+
+// Division
+// Function defination
+function division(g,h){
+    console.log(g / h);
+}
+// Function invoking/calling
+division(50,5);
